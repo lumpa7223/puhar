b80c99678ec8e4349e54e8bef055df45888a0951
Duncan Wen <dunca.wen@yitmh.com>
2019-06-11 16:26:05 +0800

    using writeFileSync to append EOF

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const puppeteer = require('puppeteer');
 const PuppeteerHar = require('puppeteer-har');
+const fs = require('fs');
+const os = require('os');
 
 (async () => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     const har = new PuppeteerHar(page);
-    await har.start({ path: 'results.har' });
-
+    //await har.start({ path: 'results1.har' });
+    await har.start();
     await page.goto(process.argv[2]);
 
-    await har.stop();
+    const ret = await har.stop();
+    fs.writeFileSync('results.har',JSON.stringify(ret)+os.EOL);
+
     await browser.close();
 })();
