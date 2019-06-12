ea14d8c2fe24d6ba85167220d81df530f13b18b1
lumpa7223 <lumpa7223@gmail.com>
2019-06-13 01:18:28 +0800

    Use datetime format to random filename and add domload pageload time in har file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const puppeteer = require('puppeteer');
 const PuppeteerHar = require('puppeteer-har');
 const fs = require('fs');
 const os = require('os');
+const moment = require('moment');
+const { extractDataFromPerformanceTiming } = require('./helpers');
 
 (async () => {
+    // headless: false --- will open browser
     const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
@@ -12,8 +15,28 @@ const os = require('os');
     await har.start();
     await page.goto(process.argv[2]);
 
+    const performanceTiming = JSON.parse(
+        await page.evaluate(() => JSON.stringify(window.performance.timing))
+    );
+    const measureTime = extractDataFromPerformanceTiming(
+        performanceTiming,
+        'responseEnd',
+        'domInteractive',
+        'domContentLoadedEventEnd',
+        'loadEventEnd'
+    );
+
     const ret = await har.stop();
-    fs.writeFileSync('results.har', JSON.stringify(ret) + os.EOL);
+
+    ret['log']['pages'].forEach((x) => {
+        x['pageTimings'] = {
+            "onContentLoad": measureTime['domContentLoadedEventEnd'],
+            "onLoad": measureTime['loadEventEnd'],
+        }
+    });
+
+    const fname = moment().format('YYYY-MM-DD_HH_mm_ss');
+    fs.writeFileSync(`${fname}.har`, JSON.stringify(ret) + os.EOL);
 
     await browser.close();
 })();
