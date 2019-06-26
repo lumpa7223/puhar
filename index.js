2429ff87dfdd557b243e97b8a6f7394865b30c43
Duncan Wen <dunca.wen@yitmh.com>
2019-06-26 18:08:11 +0800

    Can get multiple url har file with ',' seperate

--- index.js
+++ index.js
@@ -5,45 +5,51 @@ const os = require('os');
 const moment = require('moment');
 const { extractDataFromPerformanceTiming } = require('./helpers');
 
-(async () => {
+(async (urls_arg) => {
     // headless: false --- will open browser
-    const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-first-run', '--no-zygote', '--single-process', '--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-first-run', '--no-zygote', '--no-sandbox', '--disable-setuid-sandbox'] });
     const userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36';
     const page = await browser.newPage();
     await page.setUserAgent(userAgent);
-    const har = new PuppeteerHar(page);
-    //await har.start({ path: 'results1.har' });
-    await har.start();
 
-    try {
-        await page.goto(process.argv[2], { waitUntil: 'load', timeout: 60000 });
-    } catch (e) {
-        console.log(e);
-        browser.close();
+    let urls = urls_arg.split(',')
+
+    for (let i = 0; i < urls.length; i++) {
+        const har = new PuppeteerHar(page);
+        //await har.start({ path: 'results1.har' });
+        await har.start();
+        let target = urls[i].trim();
+
+        try {
+            await page.goto(target, { waitUntil: 'load', timeout: 60000 });
+        } catch (e) {
+            console.log(e);
+            browser.close();
+        }
+
+        const performanceTiming = JSON.parse(
+            await page.evaluate(() => JSON.stringify(window.performance.timing))
+        );
+        const measureTime = extractDataFromPerformanceTiming(
+            performanceTiming,
+            'responseEnd',
+            'domInteractive',
+            'domContentLoadedEventEnd',
+            'loadEventEnd'
+        );
+
+        const ret = await har.stop();
+
+        ret['log']['pages'].forEach((x) => {
+            x['pageTimings'] = {
+                "onContentLoad": measureTime['domContentLoadedEventEnd'],
+                "onLoad": measureTime['loadEventEnd'],
+            }
+        });
+        const url = target.split('//')[1];
+        const fname = `${url}_${moment().format('YYYY-MM-DD_HH_mm_ss')}`;
+        fs.writeFileSync(`./harfile/${fname}.har`, JSON.stringify(ret) + os.EOL);
     }
 
-    const performanceTiming = JSON.parse(
-        await page.evaluate(() => JSON.stringify(window.performance.timing))
-    );
-    const measureTime = extractDataFromPerformanceTiming(
-        performanceTiming,
-        'responseEnd',
-        'domInteractive',
-        'domContentLoadedEventEnd',
-        'loadEventEnd'
-    );
-
-    const ret = await har.stop();
-
-    ret['log']['pages'].forEach((x) => {
-        x['pageTimings'] = {
-            "onContentLoad": measureTime['domContentLoadedEventEnd'],
-            "onLoad": measureTime['loadEventEnd'],
-        }
-    });
-    const url = process.argv[2].split('//')[1];
-    const fname = `${url}_${moment().format('YYYY-MM-DD_HH_mm_ss')}`;
-    fs.writeFileSync(`./harfile/${fname}.har`, JSON.stringify(ret) + os.EOL);
-
     await browser.close();
-})();
+})(process.argv[2]);
